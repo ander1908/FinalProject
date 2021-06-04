335f06db66ff3df863b35eae1ce05527103be03f
Stephen Anderson <stephenganderson10@gmail.com>
2021-06-03 21:48:05 -0500

    finally, no more crazy train

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 from flask import Flask, render_template, request, redirect, url_for
-
+import joblib
 import csv
+import pandas as pd
+import numpy as np
 #import joblib
 
 app = Flask(__name__)
@@ -21,23 +23,37 @@ def search():
 
 @app.route("/<usr>")
 def user(usr):
-    csvreader = csv.reader("teams.csv")
-    next(csvreader,None)
-    for row in csvreader:
-        if row[0] == usr:
-            #grab row assign to array
-            team = row
-            cleaned_team = team.pop(0)
-            #apply model
+    df = pd.read_csv('teams.csv')
+    df = df[df['Name'] == f'{usr}']
+    df.drop('Name', axis=1, inplace=True)
+    clean_team = np.array(df.iloc[0]).reshape(1,-1)
+    loaded_model_svm=joblib.load('SVM.sav')
+    result = loaded_model_svm.predict(clean_team)
+
+    win_lose_dict = {
+        0: 'are not',
+        1: 'are'
+    }
+    txt_result = win_lose_dict[result[0]]
+    
+    return f"<h1>The {usr}s {txt_result} going to make the playoffs</h1>"
+    # csvreader = csv.reader("teams.csv")
+    # next(csvreader,None)
+    # for row in csvreader:
+    #     if row[0] == usr:
+    #         #grab row assign to array
+    #         team = row
+    #         cleaned_team = team.pop(0)
+    #         #apply model
             
-            filename = 'SVM.sav'
-            #loaded_model=joblib.load(filename)
-            result=loaded_model(cleaned_team)
-            if result[0] == 0:
-                status = " are not "
-            else:
-                status = " are "    
-            return f"<h1>The {usr}s {status}going to make the playoffs</h1>"
+    #         filename = 'SVM.sav'
+    #         loaded_model=joblib.load(filename)
+    #         result=loaded_model(cleaned_team)
+    #         if result[0] == 0:
+    #             status = " are not "
+    #         else:
+    #             status = " are "    
+    #         return f"<h1>The {usr}s {status}going to make the playoffs</h1>"
                
             
     
