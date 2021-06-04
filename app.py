b10f9020d55609231f9d0f6c3da08944b0c84706
Stephen Anderson <stephenganderson10@gmail.com>
2021-06-03 21:14:41 -0500

    continuing work

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
-from flask import Flask
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Float
+
+from sqlalchemy.ext.declarative import declarative_base
+import csv
+import joblib
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return jsonify({
-        "key1": "value1",
-        "key2": 2
-            })
+    return render_template("index.html")
+
+@app.route("/search", methods=["POST","GET"])
+def search():
+    if request.method =="POST":
+        user = request.form["nm"]
+        return redirect(url_for("user",usr=user))
+    else:    
+        return render_template("search.html")
+
+@app.route("/<usr>")
+def user(usr):
+    csvreader = csv.reader("teams.csv")
+    next(csvreader,None)
+    for row in csvreader:
+        if row[0] == usr:
+            #grab row assign to array
+            team = row
+            cleaned_team = team.pop(0)
+            #apply model
+            
+            filename = 'SVM.sav'
+            loaded_model=joblib.load(filename)
+            result=loaded_model(cleaned_team)
+            if result[0] == 0:
+                status = " are not "
+            else:
+                status = " are "    
+            return f"<h1>The {usr}s {status}going to make the playoffs</h1>"
+               
+            
+    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
